07e790e30211e6aeb339274f02e7c3c9c135bde5
HichamBarada <hichambarada13@gmail.com>
2023-05-14 22:30:36 +0300

    fixing bugs

--- backend/routes/webhooksRoutes.js
+++ backend/routes/webhooksRoutes.js
@@ -159,11 +159,8 @@ router.post("/webhook", async (req, res) => {
         new Date(Date.parse(monthName + " 1, " + year)).getMonth() + 1;
       const month = monthNumber < 10 ? "0" + monthNumber : monthNumber;
       const date = new Date(`${year}-${month}-01`);
-      const startDate= new Date(date.getUTCFullYear(),date.getUTCMonth(),1);
-      const endDate = new Date(date.getUTCFullYear,date.getUTCMonth()+1,0);
-      const start = startDate.substring(0,10);
-      const end = endDate.substring(0,10);
-      console.log("startDate",startDate);
+      const start= new Date(date.getUTCFullYear(),date.getUTCMonth(),1);
+      const end = new Date(date.getUTCFullYear,date.getUTCMonth()+1,0);
       console.log("start",start);
       console.log("end",end);
       const totalExpenses = await getExpensesByMonth(start, end);
